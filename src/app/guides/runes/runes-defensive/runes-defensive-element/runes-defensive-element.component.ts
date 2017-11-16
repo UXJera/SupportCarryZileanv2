3690987a33d43a0ebe1c846fb13a25103ee0b5c4
Jera Nyman <uxjera@gmail.com>
2017-11-16 00:54:29 -0800

    Added programmatic load for resolve masteries

--- src/app/guides/runes/runes-defensive/runes-defensive-element/runes-defensive-element.component.ts
+++ src/app/guides/runes/runes-defensive/runes-defensive-element/runes-defensive-element.component.ts
@@ -21,6 +21,79 @@ export class RunesDefensiveElementComponent implements OnInit {
     brief: '../assets/images/runes/defensive/defensive-brief-hd.jpg'
   }
 
+  resolveShortcut = '~assets/images/runes/icons/resolve/'
+
+  masteryArray = [
+    [
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-keystone-1.png',
+        desc: ''
+      },
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-keystone-2.png',
+        desc: ''
+      },
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-keystone-3.png',
+        desc: ''
+      },
+    ],
+    [
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-slot-1-1.png',
+        desc: ''
+      },
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-slot-1-2.png',
+        desc: ''
+      },
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-slot-1-3.png',
+        desc: ''
+      },
+    ],
+    [
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-slot-2-1.png',
+        desc: ''
+      },
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-slot-2-2.png',
+        desc: ''
+      },
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-slot-2-3.png',
+        desc: ''
+      },
+    ],
+    [
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-slot-3-1.png',
+        desc: ''
+      },
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-slot-3-2.png',
+        desc: ''
+      },
+      {
+        name: 'Grasp of the Undying',
+        src: './assets/images/runes/icons/resolve/resolve-slot-3-3.png',
+        desc: ''
+      },
+    ],
+  ];
+
   onToggle() {
     this.toggled = !this.toggled;
   }
