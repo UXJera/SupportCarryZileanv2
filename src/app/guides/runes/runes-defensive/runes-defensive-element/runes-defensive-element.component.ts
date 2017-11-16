244ff8b906d0eb3824cf887b46907a76aec07f32
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-11-16 13:48:04 -0800

    Cleaning up CSS for mobile

--- src/app/guides/runes/runes-defensive/runes-defensive-element/runes-defensive-element.component.ts
+++ src/app/guides/runes/runes-defensive/runes-defensive-element/runes-defensive-element.component.ts
@@ -12,6 +12,9 @@ export class RunesDefensiveElementComponent implements OnInit {
   constructor() { }
 
   toggled = false;
+  onToggle() {
+    this.toggled = !this.toggled;
+  }
 
   images: Object = {
     detailed: '../assets/images/runes/defensive/defensive-detail-sd.jpg',
@@ -23,33 +26,48 @@ export class RunesDefensiveElementComponent implements OnInit {
     brief: '../assets/images/runes/defensive/defensive-brief-hd.jpg'
   }
 
-  resolveShortcut = '~assets/images/runes/icons/resolve/'
+  toggleDescription = false;
+  onToggleDescription() {
+    this.toggleDescription = !this.toggleDescription;
+    console.log('Toggled')
+  }
 
-  masteryArray = [
-    [ new Rune('Grasp of the Undying','./assets/images/runes/icons/resolve/resolve-keystone-1.png',''),
-      new Rune('Aftershock','./assets/images/runes/icons/resolve/resolve-keystone-2.png',''),
-      new Rune('Guardian','./assets/images/runes/icons/resolve/resolve-keystone-3.png','',true),
+  resolveShortcut =  './assets/images/runes/icons/resolve/'
+  masteryArrayPrimary = [
+    [ new Rune('Grasp of the Undying', this.resolveShortcut + 'resolve-keystone-1.png',
+      'Every 4s your next attack on a champion deals bonus magic damage, heals you, and permanently increases your health.'),
+      new Rune('Aftershock', this.resolveShortcut + 'resolve-keystone-2.png',
+      'After immobilizing an enemy champion gain defenses and later deal a burst of adaptive damage around you.'),
+      new Rune('Guardian', this.resolveShortcut + 'resolve-keystone-3.png',
+      "Guard allies you cast spells on and those that are very nearby. If you or a guarded ally would take damage, you're both hasted and granted a shield.",true),
     ],
     [
-      new Rune('Unflinching','./assets/images/runes/icons/resolve/resolve-slot-1-1.png',''),
-      new Rune('Demolish','./assets/images/runes/icons/resolve/resolve-slot-1-2.png',''),
-      new Rune('Font of Life','./assets/images/runes/icons/resolve/resolve-slot-1-3.png','',true),
+      new Rune('Unflinching', this.resolveShortcut + 'resolve-slot-1-1.png',
+      'After casting a Summoner Spell, gain Tenacity and Slow Resistance for a short duration. Additionally, gain Tenacity and Slow Resistance for each Summoner Spell on cooldown.'),
+      new Rune('Demolish', this.resolveShortcut + 'resolve-slot-1-2.png',
+      'Charge up a powerful attack against a tower while near it.'),
+      new Rune('Font of Life', this.resolveShortcut + 'resolve-slot-1-3.png',
+      "Imparing the movement of an enemy champion marks them. Your allies heal when attacking champions you've marked",true),
     ],
     [
-      new Rune('Iron Skin','./assets/images/runes/icons/resolve/resolve-slot-2-1.png','',true),
-      new Rune('Mirror Shell','./assets/images/runes/icons/resolve/resolve-slot-2-2.png','',true),
-      new Rune('Conditioning','./assets/images/runes/icons/resolve/resolve-slot-2-3.png','',true),
+      new Rune('Iron Skin', this.resolveShortcut + 'resolve-slot-2-1.png',
+      'Gain 5 Armor. Heals, incluing consumables, increase your Armor by 5% temporarily.',true),
+      new Rune('Mirror Shell', this.resolveShortcut + 'resolve-slot-2-2.png',
+      'Gain 5 Magic Resist. Heals, incluing consumables, increase your Magic Resist by 5% temporarily.',true),
+      new Rune('Conditioning', this.resolveShortcut + 'resolve-slot-2-3.png',
+      'After 10 min gain +8 Armor and +8 Magic Resist and increase your Armor and Magic Resist by 5%.',true),
     ],
     [
-      new Rune('Overgrowth','./assets/images/runes/icons/resolve/resolve-slot-3-1.png',''),
-      new Rune('Revitalize','./assets/images/runes/icons/resolve/resolve-slot-3-2.png',''),
-      new Rune('Second Wind','./assets/images/runes/icons/resolve/resolve-slot-3-3.png','',true),
+      new Rune('Overgrowth', this.resolveShortcut + 'resolve-slot-3-1.png',
+      'Gain additional permanent max health when minions or monsters die near you.'),
+      new Rune('Revitalize', this.resolveShortcut + 'resolve-slot-3-2.png',
+      'Heals and shields are 5% stronger and increased by an addiotional 10% on low health targets.'),
+      new Rune('Second Wind', this.resolveShortcut + 'resolve-slot-3-3.png',
+      'After taking damage from an enemy champion heal back some of your missing health over time.',true),
     ],
   ];
 
-  onToggle() {
-    this.toggled = !this.toggled;
-  }
+
 
   ngOnInit() {
   }
