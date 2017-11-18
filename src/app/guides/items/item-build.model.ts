648c9c1533d0c26355b5147a6e475d2d7069ff33
Jera Nyman <uxjera@gmail.com>
2017-11-17 16:18:43 -0800

    Item build offensive almost done

--- src/app/guides/items/item-build.model.ts
+++ src/app/guides/items/item-build.model.ts
@@ -2,13 +2,15 @@ export class ItemBuild {
   public index: number;
   public name: string;
   public img: string;
+  public optional: boolean;
   //public description: string;
   //public selected: boolean;
 
-  constructor(index: number, name: string, imagePath: string) {
+  constructor(index: number, name: string, imagePath: string,optional:boolean=false) {
     this.index = index;
     this.name = name;
     this.img = imagePath;
+    this.optional = optional;
     //this.selected = false;
   }
 }
