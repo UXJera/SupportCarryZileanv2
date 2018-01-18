e94dedc88f3adcfac5a86dcdd4c83f04f7f89696
Jera Nyman <uxjera@gmail.com>
2018-01-18 13:06:52 -0800

    About page update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
-import {WildcardRoutingModule} from './core/wildcard-routing.module';
+import { WildcardRoutingModule } from './core/wildcard-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
